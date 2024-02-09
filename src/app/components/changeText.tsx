02f8736400f55072823383522f8ba6e5c9e52d1a
LucasTKP <lucasgeansantos@gmail.com>
2024-02-09 18:06:47 -0300

    fixed bug with scrollX

--- src/app/components/changeText.tsx
+++ src/app/components/changeText.tsx
@@ -7,11 +7,11 @@ function ChangeText() {
     return (
     <>
         {state ?
-            <p onAnimationEnd={() => setState(false)} id='notSelect' className='text-[18px] max-lg:text-[16px] max-md:text-[13px] max-sm:text-[16px] text-[#ABB2BF] border-r border-r-[#aab2bf] duration-200 animate-typewriter text-nowrap overflow-hidden'>
+            <p onAnimationEnd={() => setState(false)} style={{overflow:'hidden', whiteSpace:'nowrap'}} id='notSelect' className='text-[18px] max-lg:text-[16px] max-md:text-[13px] max-sm:text-[16px] text-[#ABB2BF] border-r border-r-[#aab2bf] duration-200 animate-typewriter'>
                 Buscando uma vaga de estágio
             </p>
         :
-            <h1 onAnimationEnd={() => setState(true)} id='notSelect' className='text-[18px] max-lg:text-[16px] max-md:text-[13px] max-sm:text-[16px] text-[#ABB2BF] border-r border-r-[#aab2bf] duration-200 animate-typewriter text-nowrap overflow-hidden'>
+            <h1 onAnimationEnd={() => setState(true)} style={{overflow:'hidden', whiteSpace:'nowrap'}} id='notSelect' className='text-[18px] max-lg:text-[16px] max-md:text-[13px] max-sm:text-[16px] text-[#ABB2BF] border-r border-r-[#aab2bf] duration-200 animate-typewriter'>
                 Estudando Next js e tailwind
             </h1>
         }
