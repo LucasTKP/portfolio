422ac61f0a85c3dbec3c28d1dbcba46b9544b379
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-02-19 22:33:56 -0300

    Update Contact.tsx

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -18,7 +18,7 @@ function Contact() {
                 </div>
                 <div className='mt-[20px] flex gap-x-[20%] max-2xl:gap-x-[15%] max-xl:gap-x-[10%] max-lg:gap-x-[5%] max-md:gap-x-[3%] items-center max-sm:flex-wrap max-sm:justify-center'>
                     <p className='text-[20px] max-xl:text-[18px] max-lg:text-[17px] max-md:text-[14px] max-sm:text-[16px] max-lsm:text-[14px] text-secondary mt-[20px]'>
-                        No momento busco minha primeira vaga na área de desenvolvimento para aplicar todos os conhecimentos que adquiri até o momento e continuar aprendendo e me aprofundando.
+                        No momento busco minha primeira vaga na área de desenvolvimento para aplicar todos os conhecimentos que adquiri até o momento e continuar aprendendo.
                     </p>
 
                     <div className='border-[2px] border-secondary rounded-[4px] p-[15px] min-w-fit max-sm:mt-[20px]'>
