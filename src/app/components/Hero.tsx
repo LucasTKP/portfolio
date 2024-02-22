e9004f99e827caf6f7b810199afb774e4bf5f499
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-02-22 15:18:34 -0300

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -15,7 +15,7 @@ function Hero() {
                     <span className='text-primary'>Lucas,</span> sou um <br /> <span className='text-primary'>Desenvolvedor</span> de <span className='text-primary'>software</span>
                 </p>
 
-                <p className='text-[20px] max-xl:text-[18px] max-lg:text-[15px] max-md:text-[13px] max-sm:text-[16px] max-lsm:text-[14px] max-sm:pr-[10px] text-[#abb2bf] mt-[40px] max-lg:mt-[20px] max-md:mt-[10px]'>Possuo experiência tanto em desenvolvimento  front-end quanto back-end, sendo capaz de criar Websites responsivos utilizando as tecnologias React/Next.</p>
+                <p className='text-[20px] max-xl:text-[18px] max-lg:text-[15px] max-md:text-[13px] max-sm:text-[16px] max-lsm:text-[14px] max-sm:pr-[10px] text-[#abb2bf] mt-[40px] max-lg:mt-[20px] max-md:mt-[10px]'>Possuo experiência em desenvolvimento front-end, mas já me aventurei no back-end, sou capaz de criar Websites responsivos utilizando as tecnologias React/Next, Node.</p>
 
                 <a href='#contact' className='max-sm:hidden font-medium relative outline outline-[2px] outline-primary outline-offset-[-1px] px-[40px] max-lg:px-[20px]  py-[10px] max-lg:py-[7px] text-[22px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] max-sm:text-[16px]  rounded-[8px] mt-auto m
             after:w-0 hover:after:w-full after:h-full after:bg-primary/10 after:absolute after:left-0 after:bottom-0 after:rounded-[8px] after:duration-200 z-0'>
