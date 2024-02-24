0a359fd2138ddf5a26a5917e693583a1596d27d2
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-02-24 11:05:18 -0300

    Update SocialMedia.tsx

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -11,7 +11,7 @@ function SocialMedia() {
         <div className='w-[4px] h-[100%] bg-[#ABB2BF]' />
         <a href='https://www.instagram.com/lucas_tkp/' target='__blank'><Image src={instagram} alt='instagram' className='w-[34px] max-lg:w-[30px] hover:brightness-75 cursor-pointer duration-200 mt-[-3px]' /></a>
         <a href='https://github.com/LucasTKP' target='__blank'><Image src={github} alt='github' className='w-[30px] max-lg:w-[26px] hover:brightness-75 cursor-pointer duration-200 my-[10px]' /></a>
-        <a href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile' target='__blank'><Image src={linkedin} alt='linkedin' className='w-[30px] max-lg:w-[26px] hover:brightness-75 cursor-pointer duration-200' /></a>
+        <a href='https://www.linkedin.com/in/lucas-gean-dos-santos/' target='__blank'><Image src={linkedin} alt='linkedin' className='w-[30px] max-lg:w-[26px] hover:brightness-75 cursor-pointer duration-200' /></a>
       </section>
   )
 }
